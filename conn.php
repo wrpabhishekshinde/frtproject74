5a50726538cbcd4b242c251ee2088cd8b9aa9d2a
abhishek shinde <wrpabhishekshinde@gmail.com>
2024-06-13 17:59:48 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,11 +3,8 @@ $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "certificate.pem", NULL, NULL);
 mysqli_real_connect($con, "frtdbserver74.mysql.database.azure.com", "gsshinde", "Pass@123", "fin_demo", 3306, MYSQLI_CLIENT_SSL);
 print_r($con);
-if(!$con)
-{
-    echo"connected successfully";
-}
-else{
-    echo"not connected";
-}
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
 ?>
