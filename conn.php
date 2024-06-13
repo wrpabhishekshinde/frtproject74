69a204e28d66da4313b6604b7239fb1172a81b73
abhishek shinde <wrpabhishekshinde@gmail.com>
2024-06-13 19:09:55 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -7,4 +7,16 @@ if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
   }
   echo "Connected successfully";
+
+$query = "SELECT * FROM category";
+    if ($result = $mysqli->query($query)) {
+        echo "Query executed successfully\n";
+        // Process the result...
+        $result->close();
+    } else {
+        echo "Error executing query: (" . $mysqli->errno . ") " . $mysqli->error;
+    }
+    
+    // Print the mysqli object again
+    print_r($mysqli);
 ?>
